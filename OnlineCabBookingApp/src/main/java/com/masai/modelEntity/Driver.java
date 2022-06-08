8f8e2155d8d7e66edda8539e16c5d431e27dc62b
najmushsaaquib <saaquibnajmush@gmail.com>
2022-06-08 13:32:54 +0530

    Data Validation Added

--- OnlineCabBookingApp/src/main/java/com/masai/modelEntity/Driver.java
+++ OnlineCabBookingApp/src/main/java/com/masai/modelEntity/Driver.java
@@ -1,11 +1,13 @@
 package com.masai.modelEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,8 +24,13 @@ public class Driver {
 	@SequenceGenerator(name="driver_generator", sequenceName = "driver_seq", allocationSize=10)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "driver_generator")
 	private Integer driverId; 
+	
 	private Integer rating;
+	
+	@Column(unique = true)
+	@NotNull
 	private String licenceId;
+	
 	
 	
 	@Embedded
--- OnlineCabBookingApp/src/main/java/com/masai/modelEntity/ModelUser.java
+++ OnlineCabBookingApp/src/main/java/com/masai/modelEntity/ModelUser.java
@@ -1,5 +1,11 @@
 package com.masai.modelEntity;
 
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +17,24 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ModelUser {
 	
+	@NotNull(message = "Username cannot be NULL")
+	@Pattern(regexp = "^[a-zA-Z0-9]{3,25}", message = "length must be >=3")
 	private String username;
+
+	
+	@NotNull(message = "Please enter the password")
+	@Pattern(regexp = "[A-Za-z0-9!@#$%^&*_]{8,15}", message = "Password must be 8-15 characters in length and can include A-Z, a-z, 0-9, or special characters !@#$%^&*_")
 	private String password;
 	private String address;
+	
+	@NotNull(message = "Please enter the mobile Number")
+	@Column(unique = true)
+	@Pattern(regexp = "[6789]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number")
 	private String mobile;
+	
+	@NotNull(message = "Please enter the emaild id")
+	@Column(unique = true)
+	@Email
 	private String email;
 	
 	
