d2a9a89b7451271f8b166f70e64f14169a3517f0
najmushsaaquib <saaquibnajmush@gmail.com>
2022-06-10 00:28:55 +0530

    Finishing Admin List Methods

--- OnlineCabBookingApp/src/main/java/com/masai/repository/CompletedTripsDao.java
+++ OnlineCabBookingApp/src/main/java/com/masai/repository/CompletedTripsDao.java
@@ -1,9 +1,13 @@
 package com.masai.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.masai.modelEntity.CompletedTrips;
 
 public interface CompletedTripsDao extends JpaRepository<CompletedTrips, Integer>{
-
+	
+	 public List<CompletedTrips> findByCustomerId(Integer customerId); 
+	
 }
--- OnlineCabBookingApp/src/main/java/com/masai/services/AdminService.java
+++ OnlineCabBookingApp/src/main/java/com/masai/services/AdminService.java
@@ -1,15 +1,25 @@
 package com.masai.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.masai.DTO.AdminDTO;
+import com.masai.exceptions.AdminException;
+import com.masai.exceptions.LoginException;
 import com.masai.modelEntity.Admin;
+import com.masai.modelEntity.CompletedTrips;
 
 public interface AdminService {
-	
-	public Admin adminRegister(Admin admin);
+
+	public Admin adminRegister(Admin admin) throws AdminException;
+
 	public Admin updatePassword(AdminDTO dto, String username, String key);
+
 	public Admin update(Admin admin, String Username, String key);
+
 	public String deleteByUsername(AdminDTO dto, String key);
-	public String logoutAdmin(String key);
+
+	public String logoutAdmin(String key) throws LoginException;
+	
+	public List<CompletedTrips> getTripsByCustomerId(Integer customerId, String key);
 }
